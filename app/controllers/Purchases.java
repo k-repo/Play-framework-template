9ad5a0780b23a794442dda6ce25554f983682fc8
Azaddyne <a.kamal@brams.net>
2014-01-12 05:24:26 +0000

    Adding categories crud customers and suppliers

---
+++ app/controllers/Purchases.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 12/01/14
+ * Time: 04:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class Purchases {
+}
